9543b91d5da74e660b747f9a237335706d6a5eb3
cnsheafe <cnsheafe@gmail.com>
2017-03-31 16:08:52 -0500

    using js to dynamically add pages to index

--- app.js
+++ app.js
@@ -1,60 +1,48 @@
 /*jshint esversion: 6*/
 function initState() {
   let state = {
-    numAnswered: 0,
     numCorrect: 0,
-    pageNum: 0
+    questionNum: 0,
+    currentQuestion: {}
   };
   return state;
 }
 
-function addCorrect(state) {
-  state.numCorrect++;
-  state.questionArr[state.arrPointer] = true;
+function loadJSON() {
+  let url = 'https://github.com/cnsheafe/quiz-project/blob/master/questions.json';
+  $.get(url, function(data) {
+    console.log(data);
+  },'json');
+  //let req = new XMLHttpRequest();
+  /*req.overrideMimeType('application/json');
+  req.responseType = 'json';
+  req.open('GET', 'questions.json');
+  let db;
+  req.onload = function() {
+    db = req.response;
+  };*/
+  //return db;
 }
 
-function goToNextPage(state) {
-  if(state.numAnswered % 5 === 0) {
-    state.pageNum++;
-    return true;
-  }
-  else {
-    return false;
-  }
+function loadQuestion(state) {
+  $.getJSON('questions.json', function(data){
+    state.currentQuestion = data;
+    console.log(data);
+  });
 }
 
-function renderQuestion(state, selector) {
-  if(selector.hasClass('correct')){
-    addCorrect(state);
-    selector.addClass('correct-highlight');
-  }
-  else {
-    selector.addClass('incorrect-highlight');
-  }
+function renderFeedback(state,selector) {
+
 }
 
-function renderNextPage(state) {
-  if(goToNextPage(state)) {
-    switch (state.pageNum) {
-      case 1:
-        $('main').append('<iframe src="pages/questions-pt1.html" width="800" height="450"></iframe>');
-        break;
-      case 2:
-        $('main').remove('iframe');
-        $('main').append('<iframe src="pages/questions-pt2.html"></frame>');
-        break;
-      case 3:
-        $('main').append('<iframe src="pages/end.html"');
-    }
-  }
-}
 
 $(function main(){
   let state = initState();
-
+  loadJSON();
+  //console.log(db);
   $('form').on('click','input', function() {
     state.numAnswered++;
-    renderQuestion(state, $(this));
+    renderFeedback(state, $(this));
   });
 
   $('form').on('submit', function(event) {
